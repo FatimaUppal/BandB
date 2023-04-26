ea6c034b5e6c0890248da38c2d371ee5f3c282d2
FatimaUppal <125939989+FatimaUppal@users.noreply.github.com>
2023-04-26 15:35:25 -0400

    Initial version of BandB generated by generator-jhipster@7.9.3

---
+++ src/main/java/com/auth/bandb/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.auth.bandb.repository;
+
+import com.auth.bandb.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/main/java/com/auth/bandb/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.auth.bandb.service.dto;
